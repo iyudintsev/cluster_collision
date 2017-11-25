c4b1da5d121c7b28cb2e1ed82a399c7e142ef5ae
iyudintsev <ivan.yudintsev18@gmail.com>
2017-11-25 16:23:11 +0100

    change main

--- main.cpp
+++ main.cpp
@@ -6,20 +6,13 @@ void clusterCollision(){
 	Model model(20, 5, 0.01);
 	
 	GenerateCluster generator(2, 2, model.min_dist);
-	generator.run(Vector3d(13, 9, 9),
-		          Vector3d(-2, 0, 0),
-		          1);
-	generator.run(Vector3d(3, 8, 8),
-		          Vector3d(2, 0, 0),
-		          1);
+	generator.run(Vector3d(13, 9, 9), Vector3d(-2, 0, 0), 1);
+	generator.run(Vector3d(3, 8, 8), Vector3d(2, 0, 0), 1);
 
 	int size = generator.get_size();
 	cout << "Number of particles: " << size << endl;
-	model.init(size,
-		       generator.coordinates, 
-		       generator.velocity, 
-		       generator.mass);
-	model.run(1000, 20, 20);
+	model.init(size, generator.coordinates, generator.velocity, generator.mass);
+	model.run(1500, 20, 20);
 }
 
 
