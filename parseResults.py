3193bb8daa89f90d6c37675ddc5f7a5f33b7accd
iyudintsev <ivan.yudintsev18@gmail.com>
2017-12-26 23:14:18 +0100

    add script for parsing results

---
+++ parseResults.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+
+
+def getInt(s):
+	return int(s.split('=')[1])
+
+def main():
+	data = []
+	with open("results.dat") as f:
+		for line in f:
+			parse_line = line.split(' ')
+			data.append((getInt(parse_line[2]), 
+						 getInt(parse_line[3])))
+	data = zip(*data)
+	plt.plot(data[0], data[1], marker='o')
+	plt.show()
+
+
+if __name__ == "__main__":
+	main()
