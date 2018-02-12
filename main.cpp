526f41ca10c85e1dfa2f40214be42fda234b71e3
iyudintsev <iyudintsev@yandex.ru>
2018-02-12 22:57:33 +0100

    update params

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 void clusterCollision(){
 	Model model(20, 5, 0.01);
 	
-	GenerateCluster generator(2, 2, model.min_dist);
+	GenerateCluster generator(4, 4, model.min_dist);
 	generator.run(Vector3d(13, 9, 9), Vector3d(-2, 0, 0), 1);
 	generator.run(Vector3d(3, 8, 8), Vector3d(2, 0, 0), 1);
 
 	int size = generator.get_size();
 	model.init(size, generator.coordinates, generator.velocity, generator.mass);
-	model.run(200, 20);
+	model.run(300, 20);
 }
 
 
