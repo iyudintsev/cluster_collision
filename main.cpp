daf190e8e421d9adbf22a2d7208519313a0e6995
iyudintsev <iyudintsev@yandex.ru>
2018-12-14 09:48:46 +0100

    replace tabs with spaces

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@
 
 
 void clusterCollision(){
-	Model model(20, 5, 0.01);
-	
-	GenerateCluster generator(4, 4, model.min_dist);
-	generator.run(Vector3d(13, 9, 9), Vector3d(-0.5, 0, 0), 1);
-	generator.run(Vector3d(3, 8, 8), Vector3d(0.5, 0, 0), 1);
+    Model model(20, 5, 0.01);
+    
+    GenerateCluster generator(4, 4, model.min_dist);
+    generator.run(Vector3d(13, 9, 9), Vector3d(-0.5, 0, 0), 1);
+    generator.run(Vector3d(3, 8, 8), Vector3d(0.5, 0, 0), 1);
 
-	int size = generator.get_size();
-	cout << "Number of particles: " << size << endl;
-	model.init(size, generator.coordinates, generator.velocity, generator.mass);
-	model.run(50, 20, 10);
+    int size = generator.get_size();
+    cout << "Number of particles: " << size << endl;
+    model.init(size, generator.coordinates, generator.velocity, generator.mass);
+    model.run(50, 20, 10);
 }
 
 
 int main(){
-	time_t t0, t1;
-	t0 = time(NULL);
-	clusterCollision();
-	t1 = time(NULL);
-	cout << "dt=" << t1 - t0 << endl;
-	return 0;
+    time_t t0, t1;
+    t0 = time(NULL);
+    clusterCollision();
+    t1 = time(NULL);
+    cout << "dt=" << t1 - t0 << endl;
+    return 0;
 }
--- model.hpp
+++ model.hpp
@@ -10,66 +10,66 @@ using namespace Eigen;
 
 
 class Particle{
-	vector<Particle*> neighbors;
+    vector<Particle*> neighbors;
 public:
-	int id;
-	double m;
-	Vector3d r, r0, f, v;
-	
-	Particle(Vector3d r_val, Vector3d v_val, double mass, int ID){
-		r = r_val;
-		r0 = r_val;
-		m = mass;
-		v = v_val;
-		f = Vector3d::Zero();
-		id = ID;
-	}
+    int id;
+    double m;
+    Vector3d r, r0, f, v;
+    
+    Particle(Vector3d r_val, Vector3d v_val, double mass, int ID){
+        r = r_val;
+        r0 = r_val;
+        m = mass;
+        v = v_val;
+        f = Vector3d::Zero();
+        id = ID;
+    }
 
-	void addNeighbor(Particle* p){
-		neighbors.push_back(p);
-	}
+    void addNeighbor(Particle* p){
+        neighbors.push_back(p);
+    }
 
-	void clearNeighbors(){
-		neighbors.clear();
-	}
+    void clearNeighbors(){
+        neighbors.clear();
+    }
 };
 
 
 class Model{
-	double L, cell_len, 
-		   h, h2_2, h_2;
-	int N, cell_num;
-	int * distances;
-	LennardJonesModel lg;
-	vector<Particle> particles;
+    double L, cell_len, 
+           h, h2_2, h_2;
+    int N, cell_num;
+    int * distances;
+    LennardJonesModel lg;
+    vector<Particle> particles;
 
-	vector<Vector3d> reflection;
-	string data_file;
+    vector<Vector3d> reflection;
+    string data_file;
 public:
-	double min_dist;
-	double Upot, Ukin;
+    double min_dist;
+    double Upot, Ukin;
 
-	Model(double l_size, int cell_number, double step);
-	~Model();
+    Model(double l_size, int cell_number, double step);
+    ~Model();
 
-	void initRefl(int l_size);
-	void createParticle(Vector3d r, Vector3d v, double m);
-	Vector3d periodicBoundCond(Vector3d r);
-	void updateNeighbors();
-	int findCell(Vector3d r);
+    void initRefl(int l_size);
+    void createParticle(Vector3d r, Vector3d v, double m);
+    Vector3d periodicBoundCond(Vector3d r);
+    void updateNeighbors();
+    int findCell(Vector3d r);
 
-	void init(int n, const vector<Vector3d>& coordinates,
-			  const vector<Vector3d>& velocity, 
-			  const vector<double>& mass);
-	
-	void updateCoordinate(Particle & p);
-	void calcForces();
-	void updateSpeed(Particle & p, const Vector3d & f0);
-	void algStep();
-	void run(int number_of_steps, int neighbors_count, int dump_num);
-	void dumpCoordinates();
+    void init(int n, const vector<Vector3d>& coordinates,
+              const vector<Vector3d>& velocity, 
+              const vector<double>& mass);
+    
+    void updateCoordinate(Particle & p);
+    void calcForces();
+    void updateSpeed(Particle & p, const Vector3d & f0);
+    void algStep();
+    void run(int number_of_steps, int neighbors_count, int dump_num);
+    void dumpCoordinates();
 
-	void calcEnergy();  // need to implement
+    void calcEnergy();  // need to implement
 };
 
 
